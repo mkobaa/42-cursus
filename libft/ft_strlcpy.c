144b4ecc2b48beb415aa5204370c63d13be7424c
Mohamed Kobaa <mkobaa@e3r9p13.1337.ma>
2023-11-09 17:56:49 +0100

    libft

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,8 +6,38 @@
 /*   By: mkobaa <mkobaa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 03:39:21 by mkobaa            #+#    #+#             */
-/*   Updated: 2023/11/09 03:39:32 by mkobaa           ###   ########.fr       */
+/*   Updated: 2023/11/09 04:41:06 by mkobaa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	if (dstsize > 0)
+	{
+		if (dstsize - 1 < src_len)
+		{
+			i = dstsize - 1;
+		}
+		else
+		{
+			i = src_len;
+		}
+		ft_memcpy(dst, src, i);
+		dst[i] = '\0';
+	}
+	return (src_len);
+}
+
+// #include <stdio.h>
+// int main()
+// {
+// 	char h[] = "hello world !";
+// 	char *d;
+// 	printf("%zu",ft_strlcpy(h, d, 20));
+// }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mkobaa <mkobaa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 15:51:23 by mkobaa            #+#    #+#             */
-/*   Updated: 2023/11/09 03:18:30 by mkobaa           ###   ########.fr       */
+/*   Updated: 2023/11/09 03:48:12 by mkobaa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
