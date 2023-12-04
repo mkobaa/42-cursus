7922bc0c7ac4dd98b1188495674ecc47ba243dd3
Mohamed Kobaa <mkobaa@e3r7p9.1337.ma>
2023-12-04 21:43:03 +0100

    create printf folder

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkobaa <mkobaa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 06:27:40 by mkobaa            #+#    #+#             */
+/*   Updated: 2023/11/24 07:02:15 by mkobaa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+#endif
