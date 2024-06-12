40b5eaaa7c1bbd5ae702d347715d7f2a8680d8d1
Mohamed Kobaa <mkobaa@e3r10p16.1337.ma>
2024-06-12 11:16:00 +0100

    t9riban kolchi msali b signal

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,94 +6,85 @@
 /*   By: mkobaa <mkobaa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 00:05:58 by mkobaa            #+#    #+#             */
-/*   Updated: 2024/06/08 00:07:02 by mkobaa           ###   ########.fr       */
+/*   Updated: 2024/06/12 11:09:41 by mkobaa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static int	skip_spaces(const char *str)
+static int skip_spaces(const char *str)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\n'
-		|| str[i] == '\t' || str[i] == '\v' || str[i] == '\r' || str[i] == '\f')
-		i++;
-	return (i);
+    int i = 0;
+    while (str[i] == ' ' || str[i] == '\n'
+        || str[i] == '\t' || str[i] == '\v' || str[i] == '\r' || str[i] == '\f')
+        i++;
+    return i;
 }
 
-int	ft_atoi(const char *str)
+int ft_atoi(const char *str)
 {
-	long	rslt;
-	int		i;
-	int		sign;
-	long	max;
+    long rslt = 0;
+    int i = skip_spaces(str);
+    int sign = 1;
+    long max = 0;
 
-	sign = 1;
-	i = skip_spaces(str);
-	rslt = 0;
-	max = 0;
-	if (str[i] == '+' || str[i] == '-')
-		if (str[i++] == '-')
-			sign = -1;
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		rslt = rslt * 10 + (str[i] - '0');
-		if (rslt < max && sign == -1)
-			return (0);
-		if (rslt < max && sign == 1)
-			return (-1);
-		i++;
-		max = rslt;
-	}
-	return ((int)(sign * rslt));
+    if (str[i] == '+' || str[i] == '-')
+        if (str[i++] == '-')
+            sign = -1;
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        rslt = rslt * 10 + (str[i] - '0');
+        if (rslt < max && sign == -1)
+            return 0;
+        if (rslt < max && sign == 1)
+            return -1;
+        i++;
+        max = rslt;
+    }
+    return (int)(sign * rslt);
 }
 
-char	*ascii_to_binary(char s)
+char *ascii_to_binary(char s)
 {
-	static char	binary[9];
-	int			i;
-
-	binary[8] = '\0';
-	i = 7;
-	while (i >= 0)
-	{
-		if (s % 2)
-			binary[i] = '1';
-		else
-			binary[i] = '0';
-		s /= 2;
-		i--;
-	}
-	return (binary);
+    static char binary[9];
+    binary[8] = '\0';
+    int i = 7;
+    while (s > 0 && i >= 0)
+    {
+        binary[i--] = (s & 1) + '0';
+        s >>= 1;
+    }
+    while (i >= 0)
+        binary[i--] = '0';
+    return binary;
 }
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int		i;
-	int		j;
-	int		pid;
-	char	*binary;
+    if (argc != 3)
+    {
+        printf("Usage: %s [PID] [MESSAGE]\n", argv[0]);
+        return 1;
+    }
 
-	i = 0;
-	if (argc == 3 && ft_atoi(argv[1]) > 0)
-	{
-		pid = ft_atoi(argv[1]);
-		while (argv[2][i])
-		{
-			binary = ascii_to_binary(argv[2][i]);
-			while (binary[j])
-			{
-				if (binary[j] == 0)
-					kill(pid, SIGUSR1);
-				else
-					kill(pid, SIGUSR2);
-				j++;
-			}
-			i++;
+    int pid = ft_atoi(argv[1]);
+    char *message = argv[2];
+
+	int i = 0;
+	while (message[i]) {
+		char *binary = ascii_to_binary(message[i]);
+		int j = 0;
+		while (binary[j]) {
+			if (binary[j] == '0')
+				kill(pid, SIGUSR1);
+			else if (binary[j] == '1')
+				kill(pid, SIGUSR2);
+			usleep(100); // Adding a small delay between signal transmissions
+			j++;
 		}
+		i++;
 	}
-	else
-		printf("%s", "please add the PID first then the text to send");
+
+
+    return 0;
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,34 +1,94 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mkobaa <mkobaa@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/07 00:12:14 by mkobaa            #+#    #+#             */
-/*   Updated: 2024/06/07 22:34:25 by mkobaa           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+#include "minitalk.h"
 #include "ft_printf/ft_printf.h"
 
-int get_pid()
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include "minitalk.h"
+#include "ft_printf/ft_printf.h"
+
+int zero_one(char s)
 {
-	pid_t pid = getpid();
-	return (pid);
+	if (s == '0')
+		return 0;
+	else
+		return 1;
 }
 
-int main() 
+int power(int i, int j)
 {
-	int pid;
-	
-	pid = get_pid();
+    int result = 1;
+    while (j > 0)
+    {
+        result *= i;
+        j--;
+    }
+    return result;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+int convert(char *s)
+{
+    int i;
+    int j;
+    int number;
+    int rslt;
+
+    rslt = 0;
+    i = 7;
+    j = 0;
+    while (i >= 0)
+	{
+        number = s[i] - '0';
+        rslt = rslt + (number * (1 << j));
+        i--;
+        j++;
+    }
+    return rslt;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void handler(int signum)
+{
+    static char str[9];
+    static int i = 0;
+
+    if (signum == SIGUSR1 || signum == SIGUSR2)
+    {
+        if (i == 8)
+        {
+            ft_printf("%c", convert(str));
+            i = 0;
+        }
+        if (signum == SIGUSR1) 
+            str[i] = '0';
+        else if (signum == SIGUSR2) 
+            str[i] = '1';
+        i++;
+    }
+}
+
+int main()
+{
+
+	int pid = getpid();
 	ft_printf("The PID is : %d\n", pid);
+	signal(SIGUSR2, handler);
+	signal(SIGUSR1, handler);
 	while (1)
 	{
-		
+		pause();
 	}
-	return 0;
+
+
 }
