2fce5be2e198e9c54be1edf33a88f12038c4d2c8
mkobaa <mohamed.kobaa@etu.uae.ac.ma>
2024-07-20 00:06:34 +0100

    push

--- push_swap/algo/sort_more.c
+++ push_swap/algo/sort_more.c
@@ -1,61 +1,63 @@
 #include "../push_swap.h"
 #include <limits.h>
 
+void to_top(t_list **stack_a, int value)
+{
+    if ((*stack_a)->index == value ) return;
+    while ((*stack_a)->index != value)
+    {
+        ra(stack_a);
+        
+    }
+}
 void push_to_b(int size, t_list **stack_a, t_list **stack_b)
 {
-    int cost_a = -1;
-    int cost_b = -1;
-    int a;
-    int b;
-    // if (size == 20)
-    while (*stack_a)
+    t_list *node = *stack_a;
+    int min = 0;
+    int moyenne = 0;
+    int i = 0;
+    while(*stack_a)
     {
-        t_list *node = *stack_a;
-        while (node->index < size / 2)
-        {
-            if (node->pos < 19)
-            {
-                cost_a = node->index;
-                a = node->index;
-                break;
-            }
-            node = node->next;
-        }
-        while (node && node->index < size / 2)
-            node = node->next;
+
         while (node)
         {
-            if (node->pos <= 19)
+            if (node->pos == 19)
             {
-                cost_b = size - node->index;
-                b = node->index;
+                min = node->value;
+            }
+            if (node->pos == 19)
+            {
+                moyenne = node->value;
             }
             node = node->next;
         }
-        printf("%d\n", cost_a);
-        printf("%d\n", cost_b);
-        int i = 0;
-        // printf("%d\n", cost_a);
-        // printf("%d\n", cost_b);
-        if (cost_a > cost_b)
+        while (i < 20)
         {
-            while (i != cost_a)
+            node = *stack_a;
+            while(node)
             {
-                ra(stack_a);
-                i++;
+                if (node->value < min)
+                {
+                    to_top(stack_a, node->index);
+                    break ;
+                }
+                node = node->next;
             }
             pb(stack_a, stack_b);
-        }
-        else
-        {
-            while(i != cost_b)
-            {
-                rra(stack_a);
-                i++;
-            }
-            pb(stack_a, stack_b);
+            i++;
         }
         add_positions(stack_a);
-        add_indices(stack_a);
+        i = 0;
+        node = *stack_a;
+    }
+}
+
+
+void push_to_a(int size, t_list **stack_a, t_list **stack_b)
+{
+    t_list *node = *stack_b;
+    while (node)
+    {
+        
     }
 }
