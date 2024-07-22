54440ea15be3cba8e1632833db64226faf114662
mkobaa <mohamed.kobaa@etu.uae.ac.ma>
2024-07-22 23:34:30 +0100

    norminette

--- push_swap/moves/moves_1.c
+++ push_swap/moves/moves_1.c
@@ -1,56 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves_1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkobaa <mkobaa@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 22:54:57 by mkobaa            #+#    #+#             */
+/*   Updated: 2024/07/22 22:54:57 by mkobaa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../push_swap.h"
 
-void sa(t_list **stack_a)
+void	sa(t_list **stack_a)
 {
-    int tmp = 0;
-    tmp = (*stack_a)->value;
-    (*stack_a)->value = (*stack_a)->next->value;
-    (*stack_a)->next->value = tmp;
-    printf("sa\n");
+	int	tmp;
+
+	tmp = (*stack_a)->value;
+	(*stack_a)->value = (*stack_a)->next->value;
+	(*stack_a)->next->value = tmp;
+	write(1, "sa\n", 3);
 }
 
-void sb(t_list **stack_b)
+void	sb(t_list **stack_b)
 {
-    int tmp = 0;
-    tmp = (*stack_b)->value;
-    (*stack_b)->value = (*stack_b)->next->value;
-    (*stack_b)->next->value = tmp;
-    printf("sb\n");
+	int	tmp;
+
+	tmp = (*stack_b)->value;
+	(*stack_b)->value = (*stack_b)->next->value;
+	(*stack_b)->next->value = tmp;
+	write(1, "sb\n", 3);
 }
 
-void ss(t_list **stack_a, t_list **stack_b)
+void	ss(t_list **stack_a, t_list **stack_b)
 {
-    sa(stack_a);
-    sa(stack_b);
-    printf("ss\n");
+	sa(stack_a);
+	sa(stack_b);
+	write(1, "ss\n", 3);
 }
 
-void ra(t_list **stack_a)
+void	ra(t_list **stack_a)
 {
-    if (ft_lstsize(*stack_a) < 2)
-        return;
-    t_list *first = *stack_a;
-    t_list *temp = *stack_a;
-    while (temp->next)
-    {
-        temp = temp->next;
-    }
-    temp->next = first;
-    *stack_a = first->next;
-    first->next = NULL;
-    printf("ra\n");
+	t_list	*first;
+	t_list	*temp;
+
+	if (ft_lstsize(*stack_a) < 2)
+		return ;
+	first = *stack_a;
+	temp = *stack_a;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = first;
+	*stack_a = first->next;
+	first->next = NULL;
+	write(1, "ra\n", 3);
 }
 
-void rb(t_list **stack_b)
+void	rb(t_list **stack_b)
 {
-    t_list *first = *stack_b;
-    t_list *temp = *stack_b;
-    if (ft_lstsize(*stack_b) < 2)
-        return;
-    while (temp->next)
-        temp = temp->next;
-    temp->next = first;
-    *stack_b = first->next;
-    first->next = NULL;
-    printf("rb\n");
+	t_list	*first;
+	t_list	*temp;
+
+	if (ft_lstsize(*stack_b) < 2)
+		return ;
+	first = *stack_b;
+	temp = *stack_b;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = first;
+	*stack_b = first->next;
+	first->next = NULL;
+	write(1, "rb\n", 3);
 }
--- push_swap/moves/moves_2.c
+++ push_swap/moves/moves_2.c
@@ -1,68 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkobaa <mkobaa@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 23:20:10 by mkobaa            #+#    #+#             */
+/*   Updated: 2024/07/22 23:20:10 by mkobaa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../push_swap.h"
 
-void pa(t_list **stack_a, t_list **stack_b)
+void	pa(t_list **stack_a, t_list **stack_b)
 {
-    if (ft_lstsize(*stack_b) == 0)
-        return;
+	t_list	*first_b;
 
-    t_list *first_b = *stack_b;
-    *stack_b = first_b->next;
-
-    first_b->next = *stack_a;
-    *stack_a = first_b;
-    printf("pa\n");
+	if (ft_lstsize(*stack_b) == 0)
+		return ;
+	first_b = *stack_b;
+	*stack_b = first_b->next;
+	first_b->next = *stack_a;
+	*stack_a = first_b;
+	write(1, "pa\n", 3);
 }
 
-
-void pb(t_list **stack_a, t_list **stack_b)
+void	pb(t_list **stack_a, t_list **stack_b)
 {
-    if (ft_lstsize(*stack_a) == 0)
-        return;
+	t_list	*first_a;
 
-    t_list *first_a = *stack_a;
-    *stack_a = first_a->next;
-
-    first_a->next = *stack_b;
-    *stack_b = first_a;
-    printf("pb\n");
-}
-void rr(t_list **stack_a, t_list **stack_b)
-{
-    ra(stack_a);
-    rb(stack_b);
-    printf("rr\n");
+	first_a = *stack_a;
+	if (ft_lstsize(*stack_a) == 0)
+		return ;
+	*stack_a = first_a->next;
+	first_a->next = *stack_b;
+	*stack_b = first_a;
+	write(1, "pb\n", 3);
 }
 
-void rra(t_list **stack_a)
+void	rr(t_list **stack_a, t_list **stack_b)
 {
-    if (ft_lstsize(*stack_a) < 2)
-        return;
-    t_list *first = *stack_a;
-    t_list *last = (*stack_a)->next;
-    while (last->next)
-    {
-        first = first->next;
-        last = last->next;
-    }
-    first->next = NULL;
-    last->next = *stack_a;
-    *stack_a = last;
-    printf("rra\n");
+	ra(stack_a);
+	rb(stack_b);
+	write(1, "rr\n", 3);
 }
 
-void rrb(t_list **stack_b)
+void	rra(t_list **stack_a)
 {
-    if (ft_lstsize(*stack_b) < 2)
-        return;
-    t_list *first = *stack_b;
-    t_list *last = (*stack_b)->next;
-    while (last->next)
-    {
-        first = first->next;
-        last = last->next;
-    }
-    first->next = NULL;
-    last->next = *stack_b;
-    *stack_b = last;
-    printf("rrb\n");
+	t_list	*first;
+	t_list	*last;
+
+	if (ft_lstsize(*stack_a) < 2)
+		return ;
+	first = *stack_a;
+	last = (*stack_a)->next;
+	while (last->next)
+	{
+		first = first->next;
+		last = last->next;
+	}
+	first->next = NULL;
+	last->next = *stack_a;
+	*stack_a = last;
+	write(1, "rra\n", 4);
+}
+
+void	rrb(t_list **stack_b)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (ft_lstsize(*stack_b) < 2)
+		return ;
+	first = *stack_b;
+	last = (*stack_b)->next;
+	while (last->next)
+	{
+		first = first->next;
+		last = last->next;
+	}
+	first->next = NULL;
+	last->next = *stack_b;
+	*stack_b = last;
+	write(1, "rrb\n", 4);
 }
